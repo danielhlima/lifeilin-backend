d0009b68230c80f24282731ce0cbe7da521ca98b
Daniel Lima <danielhenriquelima@gmail.com>
2023-09-25 15:51:02 -0300

    Project created

---
+++ src/main/java/br/com/lifeilin/LifeilinApplication.java
@@ -0,0 +1,13 @@
+package br.com.lifeilin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeilinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeilinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lifeilin/LifeilinApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lifeilin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeilinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
