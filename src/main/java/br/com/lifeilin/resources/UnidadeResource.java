198db075b25a8ade80b4d7882aa8aa35f0dbe771
Daniel Lima <danielhenriquelima@gmail.com>
2023-09-25 17:15:59 -0300

    H2 adicionado e configurado primeira entidade

--- src/main/java/br/com/lifeilin/resources/UnidadeResource.java
+++ src/main/java/br/com/lifeilin/resources/UnidadeResource.java
@@ -1,16 +1,33 @@
 package br.com.lifeilin.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.lifeilin.domain.Unidade;
 
 @RestController
 @RequestMapping(value="/unidades")
 public class UnidadeResource {
 	
 	@GetMapping
-	public String listar() {
-		return "O rest está funcionando";
+	public List<Unidade> listar() {
+		
+		Unidade uni1 = new Unidade();
+		uni1.setId(1);
+		uni1.setNome("Matriz");
+		Unidade uni2 = new Unidade();
+		uni2.setId(2);
+		uni2.setNome("Jardim Aricanduva");
+		
+		List<Unidade> lista = new ArrayList<Unidade>();
+		lista.add(uni1);
+		lista.add(uni2);
+		
+		return lista;
 	}
 
 }
