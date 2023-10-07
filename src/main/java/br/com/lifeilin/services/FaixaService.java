0a189890cbc9d20e0de8c2b6ad8c5a9c210b3427
Daniel Lima <danielhenriquelima@gmail.com>
2023-10-07 10:45:45 -0300

    Delete Faixa ok

--- src/main/java/br/com/lifeilin/services/FaixaService.java
+++ src/main/java/br/com/lifeilin/services/FaixaService.java
@@ -3,10 +3,12 @@ package br.com.lifeilin.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.lifeilin.domain.Faixa;
 import br.com.lifeilin.repositories.FaixaRepository;
+import br.com.lifeilin.services.exceptions.DataIntegrityException;
 import br.com.lifeilin.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -28,5 +30,14 @@ public class FaixaService {
 	public Faixa update(Faixa obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}catch(DataIntegrityViolationException ex) {
+			throw new DataIntegrityException("Não é possível excluir uma Faixa que possui Técnicas.");
+		}
 	}
 }
